0fbdcff6dce57c4685980d6d97d1f41297667a30
lngo0 <longngo.gm@gmail.com>
2023-06-11 21:16:19 -0500

    sample find factorial

---
+++ src/FindFactorial.java
@@ -0,0 +1,10 @@
+public class FindFactorial {
+    public static void main(String[] args) {
+        int num = 10;
+        long factorialResult = 1L;
+        for (int i = 1; i <= num; ++i) {
+            factorialResult *= i;
+        }
+        System.out.println("Factorial: "+factorialResult);
+    }
+}
