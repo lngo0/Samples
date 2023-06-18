a0f8ab66f3cf70a0eb036cb51ffb6e287b189c10
lngo0 <longngo.gm@gmail.com>
2023-06-18 10:58:48 -0500

    sample sum of two prime

---
+++ src/IsSumOfTwoPrime.java
@@ -0,0 +1,25 @@
+public class IsSumOfTwoPrime {
+    // Method to Check Prime Number
+    private static int check_prime(int num){
+        int flag = 0;
+        for (int i = 2; i <= num/2; i++) {
+            if (num % i == 0) {
+                flag = 1;
+                return 1;
+            }
+        }
+        return 0;
+    }
+    // Method to get print the prime sum
+    private static void find (int num) {
+        for (int i = 2; i <= num/2; i++) {
+            if (check_prime(i) == 0) {
+                if (check_prime(num-i) == 0)
+                    System.out.println(num + " = "+ (num-i) + " "+ i);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        find(18);
+    }
+}
