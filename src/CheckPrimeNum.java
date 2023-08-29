a646de5ba4f39e8adcf60e138cfaceccb821b268
lngo0 <longngo.gm@gmail.com>
2023-08-29 15:51:02 -0500

    update 1 liner prime checker with BigInteger

--- src/CheckPrimeNum.java
+++ src/CheckPrimeNum.java
@@ -1,19 +1,26 @@
+import java.math.BigInteger;
+
 public class CheckPrimeNum {
     public static void main(String[] args) {
 
         int num = 29;
-        boolean flag = false;
-        for (int i = 2; i <= num / 2; ++i) {
-            // Check for a non-prime number
-            if (num % i == 0) {
-                flag = true;
-                break;
-            }
-        }
+        boolean flag = isPrimeOld(num);
 
         if (!flag)
             System.out.println(num + " is a prime number.");
         else
             System.out.println(num + " is not a prime number.");
+    }
+    static boolean isPrimeOld(int num) {
+        for (int i = 2; i <= num / 2; ++i) {
+            // Check for a non-prime number
+            if (num % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean isPrimeOneLine(int num) {
+        return BigInteger.valueOf(num).isProbablePrime(1);
     }
 }
