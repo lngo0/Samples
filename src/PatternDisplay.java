4024e4ba4a40b8dc6205ce14e0065982cf8dad80
lngo0 <longngo.gm@gmail.com>
2023-10-02 21:06:25 -0500

    Full pyramid number pattern 2

--- src/PatternDisplay.java
+++ src/PatternDisplay.java
@@ -61,9 +61,9 @@ f
 
 //        squareStars(rows);
 
-        fullPyramidNum(rows, count1, count2);
+//        fullPyramidNum(rows, count1, count2);
 
-//        fullPyramidNum2(rows);
+        fullPyramidNum2(rows);
 
 //        invFullPyramidNum(rows, count1, count2);
 
@@ -575,6 +575,31 @@ f
             }
             count2 = count1 = col = 0;
 
+            System.out.println();
+        }
+    }
+
+    /*
+         Full pyramid number pattern
+                   1
+                 2 1 2
+               3 2 1 2 3
+             4 3 2 1 2 3 4
+           5 4 3 2 1 2 3 4 5
+     */
+    private static void fullPyramidNum2(int n) {
+        for (int row = 1; row <= n; ++row) {
+            //spacing
+            for (int space = 0; space < n - row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
             System.out.println();
         }
     }
